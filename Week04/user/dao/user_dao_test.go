bb7da6c4f17d19525bc2412ded92bd6c982b893f
chenty <cty898@126.com>
2020-12-14 21:13:50 +0800

    上传第一版

---
+++ Week04/user/dao/user_dao_test.go
@@ -0,0 +1,46 @@
+package dao
+
+import (
+	"testing"
+)
+
+func TestUserDAOImpl_Save(t *testing.T) {
+	userDAO := &UserDAOImpl{}
+
+	err := InitMysql("127.0.0.1", "3306", "root", "123456", "user")
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+	user := &UserEntity{
+		Username:"cty",
+		Password:"cty",
+		Email:"cty@mail.com",
+	}
+
+	err = userDAO.Save(user)
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+	t.Logf("new User ID is %d", user.ID)
+
+}
+
+func TestUserDAOImpl_SelectByEmail(t *testing.T) {
+
+	userDAO := &UserDAOImpl{}
+
+	err := InitMysql("127.0.0.1", "3306", "root", "123456", "user")
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	user, err := userDAO.SelectByEmail("cty@mail.com")
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+	t.Logf("result uesrname is %s", user.Username)
+}
---
+++ Week04/user/go.mod
@@ -0,0 +1,18 @@
+module github.com/cty898/Go-000/Week04/user
+
+go 1.14
+
+require (
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-kit/kit v0.10.0
+	github.com/go-redsync/redsync v1.4.2
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gomodule/redigo v2.0.0+incompatible
+	github.com/gorilla/mux v1.7.4
+	github.com/hashicorp/consul/api v1.3.0
+	github.com/jinzhu/gorm v1.9.14
+	github.com/cty898/Go-000/Week04/user v0.0.0-20201022121120-99cece9b254e
+	github.com/pborman/uuid v1.2.0
+	github.com/prometheus/client_golang v1.3.0
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+)
